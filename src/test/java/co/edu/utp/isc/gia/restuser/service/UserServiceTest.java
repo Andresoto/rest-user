8ceb86ef61868d737ccaca106cfe81b99a28fa03
Andres Soto <zuluandres27@gmail.com>
2021-04-06 14:45:48 -0500

    Practica del dia 4 completada

--- src/test/java/co/edu/utp/isc/gia/restuser/service/UserServiceTest.java
+++ src/test/java/co/edu/utp/isc/gia/restuser/service/UserServiceTest.java
@@ -5,10 +5,20 @@
  */
 package co.edu.utp.isc.gia.restuser.service;
 
+import co.edu.utp.isc.gia.restuser.data.entity.User;
+import co.edu.utp.isc.gia.restuser.data.repository.UserRepository;
 import co.edu.utp.isc.gia.restuser.web.dto.UserDto;
 import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import org.mockito.Mockito;
+import static org.mockito.Mockito.when;
+import org.modelmapper.ModelMapper;
+
+
 
 /**
  *
@@ -18,18 +28,37 @@ public class UserServiceTest {
     
     public UserServiceTest() {
     }
+    
+    private UserRepository userRepository;  //Objeto imitador, objeto dummie
+    private ModelMapper modelMapper;
+    
+    @BeforeEach
+    public void setUp(){
+        userRepository = Mockito.mock(UserRepository.class);
+        modelMapper = new ModelMapper();
+    }
 
     @Test
     public void testSave() {
-        System.out.println("save");
-        UserDto user = null;
-        UserService instance = null;
-        UserDto expResult = null;
-        UserDto result = instance.save(user);
-        assertEquals(expResult, result);
-        fail("The test case is a prototype.");
+        
+         System.out.println("Save");
+        
+        User resulted = new User(1L, "andresoto", "123", "Andres", "a.soto@utp.edu.co");
+        when(userRepository.save(any(User.class))).thenReturn(resulted);
+        
+        UserDto user = new UserDto(1L, "andresoto", "123", "Andres", "a.soto@utp.edu.co");
+        UserService instance = new UserService(userRepository, modelMapper);
+        UserDto result = instance.save(user);        
+        
+        UserDto expResult = new UserDto(1L, "andresoto", "123", "Andres", "a.soto@utp.edu.co");      
+        
+        assertEquals(expResult.getUsername(), result.getUsername());
+        assertEquals(expResult.getEmail(), result.getEmail());
+        assertEquals(expResult.getPassword(), result.getPassword());
+        assertEquals(expResult.getId(), result.getId());
+        
     }
-
+    /*
     @Test
     public void testListAll() {
         System.out.println("listAll");
@@ -73,5 +102,5 @@ public class UserServiceTest {
         assertEquals(expResult, result);
         fail("The test case is a prototype.");
     }
-    
+    */
 }
